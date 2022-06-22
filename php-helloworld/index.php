3bfc3184ef458d5350d00277c9ed0e5641ae0699
Student User <student@workstation.lab.example.com>
2022-06-22 11:54:39 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "\n";
-print "A change is in the air!\n";
+echo "Hello, World! php version is " . PHP_VERSION . "\n";
+echo "\n";
+echo "A change is in the air!\n";
 ?>
